79282b33c3b5fb05e56cbb6cc83ed65404963223
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-05-31 13:17:12 -0300

    Add project files.

---
+++ TwentyOne/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+        public abstract void Play();
+        public virtual void Listplayers()
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
